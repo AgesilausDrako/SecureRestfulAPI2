0d7f16406ef49c4de45b9366ca1ddec1ab3926f9
AgesilausDrako <agesilaus@yahoo.com>
2018-06-23 18:03:57 -0400

    added UserModel

---
+++ src/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+import bcrypt from 'bcrypt';
+
+export const UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    hashPassword: {
+        type: String,
+        required: true
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+UserSchema.methods.comparePassword = (password, hashPassword) => {
+    return bcrypt.compareSync(password, hashPassword);
+};
